d421165cd8ea28b71798c8887ca0897051764ed9
K Griffin <k.griffin@ufl.edu>
2025-07-31 18:35:11 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -6,6 +6,18 @@ One function per operation, in order.
 """
 import math
 
+# KG code 559pm
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except ValueError:
+        if a < 0:
+            raise ValueError("a must be greater than Zero")
+
+def hypotenuse(a, b):
+        return math.hypot(a, b)
+
+
 # First example
 def add(a, b):
     return a + b
