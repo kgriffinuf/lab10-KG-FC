f325177fc6f298c58ab3f8a027abdec7106aa9aa
K Griffin <k.griffin@ufl.edu>
2025-07-31 18:47:11 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -17,36 +17,11 @@ def square_root(a):
 def hypotenuse(a, b):
         return math.hypot(a, b)
 
-
-# First example
-def add(a, b): 
-	return a + b
-
-def sub(a, b): 
-	return a - b
-
-def mul(a, b): 
-	return a * b
-
 def div(a, b): 
 	try:
 		return b / a
 	except ZeroDivisionError:
 		raise ZeroDivisionError("Cannot divide by zero")
-
-def log(a, b):
-    try:
-        return math.log(b, a)
-	except ValueError as error:
-        if b <= 0:
-            raise ValueError("Cannot logarithm of zero")
-        if a <= 1:
-            raise ValueError("Invalid parameter")
-        else:
-            raise error
-
-def exp(a, b): 
-	return a ** b
 
 def add(a, b):
     return a + b
