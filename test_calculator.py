2b8d44aa88b8cf45f43c63c1d8a177f0623127b7
K Griffin <k.griffin@ufl.edu>
2025-07-31 18:58:25 -0400

    test_calculator initial update

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,10 @@
+# https://github.com/kgriffinuf/lab10-KG-FC.git
+# Partner 1: Kenneth Griffin
+# Partner 2: Francisco Carmona
+
 import unittest
 from calculator import *
+import math
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
@@ -10,13 +15,31 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    ####### Partner 1
+    def test_multiply(self): # 3 assertions
+        # check if the results are equal to the expected
+        expected = self.a * self.b
+        result = self.mul
+        self.assertEqual(result, expected)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+        # check if result is an integer
+        self.assertIsInstance(result, int)
+
+        # verify if the product(result) is greater than both variables a and b
+        self.assertTrue(result >= self.a and result >= self.b)
+
+    def test_divide(self): # 3 assertions
+        # ensure a is not equal to zero
+        assert self.a != 0, "Zero Division Error"
+
+        # ensure the result of div method is equal to the expected
+        expected = self.b / self.a
+        result = self.div
+        self.assertEqual(result, expected)
+
+        # ensure the result is a float
+        self.assertIsInstance(result, float)
+    #########################
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -33,23 +56,44 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
     
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    ####### Partner 1
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            math.log(self.a, self.b)
+        # call log function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     logarithm(0, 5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        with self.assertRaises(ValueError):
+            self.a != 0
+        with self.assertRaises(ValueError):
+            self.b != 0
+        expected = math.hypot(self.a, self.b)
+        result = self.hypotenuse
+        self.assertEqual(result, expected)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+
+    def test_sqrt(self): # 3 assertions
+        # Test for invalid argument, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #    square_root(NUM)
+        # Test basic function
+        expected = math.sqrt(self.a)
+        result = math.sqrt(self.a)
+
+        # a is greater than zero
+        with self.assertRaises(ValueError):
+            self.a < 0
+
+        # result is a float
+        self.assertIsInstance(result, float)
+
+        # value error for assertRaises
+        with self.assertRaises(ValueError):
+            math.sqrt(self.a, self.b)
+
+    #########################
 
 # Do not touch this
 if __name__ == "__main__":
